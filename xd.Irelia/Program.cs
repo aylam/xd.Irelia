e6bbe1795d3f94cacd3cb976f50dab7c6443ab97
xd <ensoulxd@outlook.com>
2019-09-14 21:07:52 +0300

    Add project files.

---
+++ xd.Irelia/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using EnsoulSharp.SDK;
+using xd.Irelia.Properties;
+
+namespace xd.Irelia
+{
+    class Irelia
+    {
+        static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += OnGameLoad;
+        }
+        [PermissionSet(SecurityAction.Assert, Unrestricted = true)]
+        private static void OnGameLoad()
+        {
+            try
+            {
+                var a = Assembly.Load(Resources.Irelia);
+                var myType = a.GetType("Irelia.Irelia");
+                var methon = myType.GetMethod("Main", BindingFlags.Public | BindingFlags.Static);
+
+                if (methon != null)
+                {
+                    methon.Invoke(null, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
